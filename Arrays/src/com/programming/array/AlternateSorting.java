4a3af713d90ecae7fe2294ad1805b3b7b65a2cb8
unknown <dharmendra.ksonu@gmail.com>
2020-07-21 19:53:53 +0530

    Rearrange Array Alternately || Alternative Sorting

---
+++ Arrays/src/com/programming/array/AlternateSorting.java
@@ -0,0 +1,41 @@
+package com.programming.array;
+
+import java.util.Arrays;
+
+public class AlternateSorting {
+
+	public static int[] rearrangeArray(int arr[]) {
+
+		int len = arr.length;
+		int temp[] = new int[len];
+		int start = 0, end = arr.length - 1;
+		int k = 0;
+		Arrays.sort(arr);
+		while (start < end) {
+			temp[k++] = arr[end--];
+			temp[k++] = arr[start++];
+
+		}
+		// Mentioned below condition to handle odd Number of elements in an array
+		if (len % 2 != 0) {
+			temp[k++] = arr[start];
+
+		}
+		return temp;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int arr[] = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+		int altSortedArray[] = rearrangeArray(arr);
+
+		for (int i : altSortedArray) {
+			System.out.print(i + " ");
+
+		}
+
+	}
+
+}
