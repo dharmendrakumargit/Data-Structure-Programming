c24dfecc056753e3517d93de8f3183c76499a5cd
Dharmendra Kumar <dharmendra.ksonu@gmail.com>
2021-02-20 13:43:11 +0530

    Write a program in java to check whether given String is Palindrome or Not

---
+++ String/com/programming/string/PalindromeCheck.java
@@ -0,0 +1,24 @@
+package com.programming.string;
+
+/* Author Dharmendra Kumar 
+   20  Feb 2021
+
+   Write a program to check whether given String is Palindrome or Not  ?
+
+*/
+
+public class PalindromeCheck {
+	public static void main(String[] args) {
+		String str = "madam";
+		System.out.println(isPalindrome(str));
+	}
+
+	public static boolean isPalindrome(String str) {
+		String strRev = new StringBuffer(str).reverse().toString();
+		if (strRev.equalsIgnoreCase(str)) {
+			return true;
+		}
+		return false;
+	}
+
+}
